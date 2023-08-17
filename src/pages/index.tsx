902973d9234839e878fcf1d457d5b2e045e8c6e0
MercyKorir <mcthegreat78@gmail.com>
2023-08-17 05:44:43 +0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+    <h1>This is the Bitnine's Website clone</h1>
+    </>
+  )
+}
