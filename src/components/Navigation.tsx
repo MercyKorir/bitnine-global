dd0fbdfc8effc797d18460a3106db7c1ede49b92
MercyKorir <mcthegreat78@gmail.com>
2023-08-23 05:24:40 +0300

    fix: layout issues in navbar hamburger menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Navigation.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSearch, faPerson } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faPerson, faBars } from '@fortawesome/free-solid-svg-icons';
 import { useSession } from 'next-auth/react';
 import {
   faFacebook,
@@ -20,7 +20,8 @@ library.add(
   faLinkedinIn,
   faMedium,
   faSearch,
-  faPerson
+  faPerson,
+  faBars
 );
 
 type Props = {};
@@ -28,6 +29,11 @@ type Props = {};
 const Navigation: React.FC<{}> = (props: Props) => {
   const { data: session } = useSession();
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleScroll = () => {
     if (window.scrollY > 0) {
@@ -47,7 +53,9 @@ const Navigation: React.FC<{}> = (props: Props) => {
   }, [isScrolled]);
   return (
     <nav
-      className={`flex flex-col bg-black min-h-fit w-full fixed top-0 z-50 ${isScrolled ? 'pb-3' : 'pb-5'}`}
+      className={`flex flex-col bg-black min-h-fit w-full fixed top-0 z-50 ${
+        isScrolled ? 'pb-3' : 'pb-5'
+      }`}
     >
       <div
         className={`h-[45px] bg-color-gradient w-full text-white hover:text-[#fac433] justify-center items-center text-[18px] transform transition-all duration-300 ease-in-out ${
@@ -56,7 +64,7 @@ const Navigation: React.FC<{}> = (props: Props) => {
       >
         <a href="">AgensSQL로 보안 기능들을 강화시키세요.</a>
       </div>
-      <div className="h-1/3 flex flex-row justify-end px-1 items-center text-white">
+      <div className="h-1/3 hidden lg:flex lg:flex-row justify-end px-1 items-center text-white">
         <div className="">
           <a href="" className="hover:text-slate-400">
             <FontAwesomeIcon className="w-[18px] h-[18px]" icon={faSearch} />
@@ -101,7 +109,8 @@ const Navigation: React.FC<{}> = (props: Props) => {
           </a>
         </div>
       </div>
-      <div className=" h-2/3 flex flex-row items-center justify-between text-white pl-8">
+
+      <div className=" h-2/3 flex flex-row w-full flex-wrap lg:flex-nowrap items-center justify-between text-white px-10 lg:px-0 lg:pl-8 pt-4 lg:pt-0">
         <div className="">
           <div>
             <a href="/">
@@ -116,51 +125,61 @@ const Navigation: React.FC<{}> = (props: Props) => {
             </a>
           </div>
         </div>
-        <div className=" mr-5">
-          <ul className="flex flex-row text-lg items-center">
-            <li className="ml-5">
+        <button
+          className=" ml-auto mr-5 inline-flex cursor-pointer text-white lg:hidden"
+          onClick={handleClick}
+        >
+          <FontAwesomeIcon className="w-[40px] h-[40px]" icon={faBars} />
+        </button>
+        <div className=" mr-5 w-full lg:w-auto">
+          <ul
+            className={`${
+              isOpen ? 'hidden' : ''
+            } lg:flex lg:flex-row text-lg items-center lg:mt-0 mt-6`}
+          >
+            <li className="lg:ml-5 ml-0 mb-5 lg:mb-0">
               <a href="" className="hover:text-[#fac433]">
                 <span>PRODUCTS</span>
               </a>
             </li>
-            <li className="ml-5">
+            <li className="lg:ml-5 ml-0 mb-5 lg:mb-0">
               <a href="" className="hover:text-[#fac433]">
                 <span>USE CASES</span>
               </a>
             </li>
-            <li className="ml-5">
+            <li className="lg:ml-5 ml-0 mb-5 lg:mb-0">
               <a href="" className="hover:text-[#fac433]">
                 <span>SERVICES</span>
               </a>
             </li>
-            <li className="ml-5">
+            <li className="lg:ml-5 ml-0 mb-5 lg:mb-0">
               <a href="" className="hover:text-[#fac433]">
                 <span>RESOURCES</span>
               </a>
             </li>
-            <li className="ml-5">
+            <li className="lg:ml-5 ml-0 mb-5 lg:mb-0">
               <a href="" className="hover:text-[#fac433]">
                 <span>BLOG</span>
               </a>
             </li>
-            <li className="ml-5">
+            <li className="lg:ml-5 ml-0 mb-5 lg:mb-0">
               <a href="" className="hover:text-[#fac433]">
                 <span>COMPANY</span>
               </a>
             </li>
-            <li className="ml-5">
+            <li className="lg:ml-5 ml-0 mb-5 lg:mb-0">
               <a href="" className="hover:text-[#fac433]">
                 <span>IR</span>
               </a>
             </li>
-            <li className="ml-5">
+            <li className="lg:ml-5 ml-0 mb-5 lg:mb-0">
               <button className=" bg-[#022EA9] px-3 py-1 rounded-[0.225rem]">
                 <a href="" className="hover:text-[#fac433]">
                   TRY FREE
                 </a>
               </button>
             </li>
-            <li className="ml-5">
+            <li className="lg:ml-5 ml-0 mb-2 lg:mb-0">
               {session ? (
                 <Link href="/profile">
                   <FontAwesomeIcon
@@ -171,7 +190,7 @@ const Navigation: React.FC<{}> = (props: Props) => {
               ) : (
                 <Link href="/login">
                   <button className=" bg-[#022EA9] px-3 py-1 rounded-[0.225rem] hover:text-[#fac433]">
-                    Login
+                    LOGIN
                   </button>
                 </Link>
               )}
