c289e51ac296bb7a57cea1ec188dfae8d427223d
MercyKorir <mcthegreat78@gmail.com>
2023-08-23 07:56:50 +0300

    feat: responsiveness of features component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -5,16 +5,16 @@ type Props = {};
 
 const Features: React.FC<{}> = (props: Props) => {
   return (
-    <section className="bg-white max-h-[100vh] py-20">
+    <section className="bg-white max-h-[100vh] py-16 px-2 xs:px-0 xs:py-20">
       <div className="flex flex-col items-center">
-        <div className="flex flex-col mb-11">
+        <div className="flex flex-col mb-9 xs:mb-11">
           <div className=" mb-4">
-            <h2 className="text-center text-[34px]">
+            <h2 className="text-center text-[30px] xs:text-[34px]">
               <strong>Key features</strong>
             </h2>
           </div>
           <div>
-            <p className="text-center text-[#666666]">
+            <p className="text-center text-[12px] text-[#666666] xs:text-base">
               Manage your data with Agens Enterprise Package
               <br />
               Essential enterprise features such as high availability and
@@ -30,6 +30,7 @@ const Features: React.FC<{}> = (props: Props) => {
             alt="Features Image"
             width={1100}
             height={432}
+            className='w-[300px] h-[135px] xxs:w-[350px] xxs:h-[175px] xs:w-[395px] xs:h-[185px] sm:w-[600px] sm:h-[252px] lg:w-[800px] lg:h-[350px] xl:w-[1100px] xl:h-[432px]'
           />
         </div>
       </div>
