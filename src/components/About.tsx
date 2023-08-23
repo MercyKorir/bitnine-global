217f874d0782804a82310e792483a50cd86d9448
MercyKorir <mcthegreat78@gmail.com>
2023-08-23 07:39:03 +0300

    feat: responsiveness of About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,8 +5,8 @@ type Props = {};
 
 const About: React.FC<{}> = (props: Props) => {
   return (
-    <section className="min-h-[100vh]  bg-[#F3F6FB] py-24">
-      <div className="flex justify-center">
+    <section className="min-h-fit  bg-[#F3F6FB] py-20 xs:py-24">
+      <div className="flex justify-center items-center">
         <Image
           src={
             'https://bitnine.net/wp-content/uploads/2022/04/img_diagram_eng-e1651143428347.png'
@@ -14,6 +14,7 @@ const About: React.FC<{}> = (props: Props) => {
           alt="Package Image"
           width={900}
           height={752}
+          className='w-[300px] h-[272px] xxs:w-[350px] xxs:h-[270px] xs:w-[400px] xs:h-[350px] sm:w-[650px] sm:h-[450px] md:w-[800px] md:h-[652px] xl:w-[900px] xl:h-[752px]'
         />
       </div>
     </section>
